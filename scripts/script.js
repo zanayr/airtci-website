d334fb9402464fad7fdb01e2f413c7d0a49dfa80
Ryan Fickenscher <rsficken@gmail.com>
2020-04-28 09:52:38 -0700

    refactoring

--- scripts/script.js
+++ scripts/script.js
@@ -1,52 +1,31 @@
-(function () {
-    const slides = document.getElementsByClassName('slide');
-    const count = slides.length;
-    const delay = 1000;
-    let current = 0;
-    let last;
-    
-    document.getElementById('nav-open').addEventListener('click', e => {
+(function() {
+    const nav = document.getElementById('nav');
+    const contact = document.getElementById('contact');
+    const openNav = document.getElementById('nav-open');
+    const navLinks = document.getElementsByClassName('nav-link');
+    const openContacts = document.getElementsByClassName('contact-open');
+    const closeNav = document.getElementById('nav-close');
+    const closeContact = document.getElementById('contact-close');
+
+    openNav.addEventListener('click', e => {
         e.preventDefault();
-        document.getElementById('nav').classList.add('active');
+        nav.classList.add('active');
     });
-
-    document.getElementById('nav-close').addEventListener('click', e => {
+    closeNav.addEventListener('click', e => {
         e.preventDefault();
-        document.getElementById('nav').classList.remove('active');
+        nav.classList.remove('active');
     });
-
-    document.getElementById('contact-close').addEventListener('click', e => {
+    for (link of navLinks)
+        link.addEventListener('click', e => {
+            nav.classList.remove('active');
+        });
+    for (button of openContacts)
+        button.addEventListener('click', e => {
+            e.preventDefault();
+            contact.classList.add('active');
+        });
+    closeContact.addEventListener('click', e => {
         e.preventDefault();
-        document.getElementById('contact').classList.remove('active');
+        contact.classList.remove('active');
     });
-
-    for (element of document.getElementsByClassName('button')) {
-        if (element.attributes['data-action']) {
-            element.addEventListener('click', e => {
-                e.preventDefault();
-                document.getElementById('contact').classList.add('active');
-            });
-        }
-    }
-    for (element of document.getElementsByClassName('nav-button')) {
-        element.addEventListener('click', e => {
-            document.getElementById('nav').classList.remove('active');
-        })
-    }
-
-    function carosel() {
-        setInterval(() => {
-            last = current;
-            current = current >= count - 1 ? 0 : current + 1;
-
-            setTimeout(() => {
-                slides[last].classList.remove('active');
-            }, delay);
-            slides[last].style.zIndex = -1;
-
-            slides[current].classList.add('active');
-            slides[current].style.zIndex = 1;
-        }, 3000);
-    }
-    carosel();
 }());
