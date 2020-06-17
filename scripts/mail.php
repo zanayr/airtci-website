3d0bd8dfd28e5f8fbf542a4115878989ad1f30f8
Ryan Fickenscher <rsficken@gmail.com>
2020-06-17 00:34:57 -0700

    contact scripts

---
+++ scripts/mail.php
@@ -0,0 +1,36 @@
+<?php
+if ($_POST["name"]) {
+    // $to = "hello@airtci.com";
+    $to = "mfickens@airtci.com";
+    $subject = "Message from airtci.com";
+    $headers = "From: airtci";
+    
+    $name = filter_input(INPUT_POST, 'name', FILTER_SANITIZE_STRING);
+    $phone = filter_input(INPUT_POST, 'phone', FILTER_SANITIZE_STRING);
+    $email = filter_input(INPUT_POST, 'email', FILTER_VALIDATE_EMAIL);
+    $service = filter_input(INPUT_POST, 'service', FILTER_SANITIZE_STRING);
+    $message = filter_input(INPUT_POST, 'message', FILTER_SANITIZE_STRING);
+    
+    $content = "Hello dad, I have successfully published your site to airtci.com and wired up the email script.\r\n";
+    $content .= "Please call me around noon tomorrow.\r\n\n";
+
+    $content .= "Hello,\r\n\n";
+    $content .= "You have recieved a request for consultation from " . $name . " about " . $service . ".\r\n\n";
+    $content .= "Contact information:\r\n";
+    $content .= "Email: " . $email . "\r\n";
+    $content .= "Phone: " . $phone . "\r\n\n";
+    if ($message) {
+        $content .= "Included Message:\r\n";
+        $content .= $message . "\r\n\n";
+    }
+    $content .= "Best,\r\n";
+    
+    $captcha = $_POST["captcha"];
+    $secret = "6LcHvaUZAAAAAM2sGA9fTh3u5HrCqbSUA7hUnjuA";
+    $response = file_get_contents("https://www.google.com/recaptcha/api/siteverify?secret=" . $secret . "&response=" . $captcha . "&remoteip=" . $_SERVER['REMOTE_ADDR']);
+    
+    if($response.success == true) {
+        $sent = mail($to, $subject, $content, $headers);
+    }
+}
+?>
--- scripts/script.js
+++ scripts/script.js
@@ -8,7 +8,30 @@
         if (aside.id === 'contact') document.body.classList.add('lock');
         aside.classList.add('active');
     }
-
+    // Submit function
+    // Should submit the contact form after running the reCaptcha API
+    function submit(e) {
+        e.preventDefault();
+        grecaptcha.ready(() => {
+            grecaptcha.execute('6LcHvaUZAAAAAMUFMcYoyZJ1I9Tmx5RITPfRgDNb', {action: 'submit'})
+                .then(token => {
+                    let data;
+                    document.getElementById('captcha').value = token;
+                    // serialize the data
+                    data = new URLSearchParams(new FormData(document.getElementById('form')));
+                    fetch('./scripts/mail.php', {method: 'POST', body: data})
+                        .then(response => {
+                            document.getElementById('form').reset();
+                            close(document.getElementById('contact'));
+                            alert("Your information has been sent to AirTCI.");
+                        })
+                        .catch(error => {
+                            document.getElementById('form').reset();
+                            alert("There was an error, please try again.");
+                        });
+                });
+        });
+    }
     // Event delegation
     document.addEventListener('click', event => {
         if (event.target.classList.contains('nav-link')) return;
@@ -17,19 +40,18 @@
         if (event.target.id == 'close-nav') return close(document.getElementById('nav'));
         if (event.target.classList.contains('contact-link')) return open(document.getElementById('contact'));
         if (event.target.id == 'close-contact') return close(document.getElementById('contact'));
+        return;
     });
     window.addEventListener('load', () => {
         document.getElementById('year').innerHTML = new Date(Date.now()).getFullYear().toString();
         setTimeout(() => {
-            // Unobscure the phone and email links to protect against website scrapers
+            // Unobscure the email links to protect against website scrapers
             const emailLinks = document.getElementsByClassName('email-link');
-            const phoneLink = document.getElementById('phone');
             for (link of emailLinks) {
                 link.innerHTML = `${(29234652).toString(36)}@airtci.com`;
                 link.href = `mailto:${(29234652).toString(36)}@airtci.com`;
             }
-            phoneLink.innerHTML = `+1 602 ${parseInt("33a", 16)} 0170`;
-            phoneLink.href = `tel:1602${parseInt("33a", 16)}0170`;
         }, 250);
-    }) 
+    });
+    document.getElementById('submit').addEventListener('click', submit);
 }());
